71088b9a423a5af3b113ec7693df7f8c1a8bf1f9
Ivan Puzek <ivan.puzek@gmail.com>
2016-08-01 21:35:58 +0200

    df subset fertig

--- fiddle__adv-r.R
+++ fiddle__adv-r.R
@@ -43,9 +43,26 @@ df["x"] # like a list --- NO simplification.
 
 ###exercises
 xx <- 1:5; xx[NA]
-xx <- 1:5; xx[NA_real_] #a bo?
+xx <- 1:5; xx["NA"]
+xx <- 1:5; xx[is.na(xx)]
+xx <- 1:5; xx[NA_real_] # subsettiranje s NA vulgaris vraća NA
 
 
+df2 <- data_frame(x = c(NA,1,2), y = c("a", NA, "NA"), z = c(FALSE, TRUE, NA))
+df.df <- data.frame(x = c(NA,1,2), y = c("a", NA, "NA"), z = c(FALSE, TRUE, NA),
+                    stringsAsFactors = FALSE)
+
+df2[!is.na(df2)] # ne šljaka, column indexing s matricom
+df2[!is.na(df2), ] # row indexing vraća čudan rezultat
+
+df2[!is.na(df2$x)] # bzvz, vraća NOT-x
+df2[!is.na(df2$x), ] # row indexing je suvisao
+
+df3 <- df2
+df3[is.na(df3)] <- 0; print(df3) # zašto ovo radi?...
+df2[is.na(df2)] # ...a ovo ne? ... dijelom zato što je tibble...
+
+df.df[is.na(df.df)] # bit će zato što tibble "Only recycles length 1 inputs."
 
 
 
